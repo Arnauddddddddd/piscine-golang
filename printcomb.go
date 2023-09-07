bb687ae6505bc269e27ea82ea45c99401b97ac76
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-07 23:48:36 +0200

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -3,20 +3,17 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	listeChiffre := [10]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
-	listeNombres := []string{}
-
+	listeChiffre := [10]rune{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 10; j++ {
 			for k := 0; k < 10; k++ {
 				if i < j && j < k {
-					listeNombres = append(listeNombres, (listeChiffre[i] + listeChiffre[j] + listeChiffre[k]))
+					z01.PrintRune(listeChiffre[i])
+					z01.PrintRune(listeChiffre[j])
+					z01.PrintRune(listeChiffre[k])
+					z01.PrintRune('\n')
 				}
 			}
 		}
-	}
-	for nombre := range listeNombres {
-		print(listeNombres[nombre], " ")
-		z01.PrintRune('\n')
 	}
 }
