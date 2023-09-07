35124f0de9c6e386f37ba266e366599bb15a4450
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-07 23:05:43 +0200

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -2,7 +2,7 @@ package main
 
 import (github.com/01-edu/z01")
 
-func main() {
+func PrintComb() {
 	listeChiffre := [...]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
 	listeNombres := []string{}
 
