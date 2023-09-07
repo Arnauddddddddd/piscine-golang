bbd5efb738cdb89400d368a9d771f2799b02b1e6
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-07 23:03:48 +0200

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -1,18 +1,16 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import (github.com/01-edu/z01")
 
 func main() {
-	var listeChiffre = [...]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
-	var listeNombres = []string{}
+	listeChiffre := [...]string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}
+	listeNombres := []string{}
 
 	for i := 0; i < 10; i++ {
 		for j := 0; j < 10; j++ {
 			for k := 0; k < 10; k++ {
-				if (i < j && j < k) {
-					listeNombres = append(listeNombres,(listeChiffre[i]+listeChiffre[j]+listeChiffre[k]))
+				if i < j && j < k {
+					listeNombres = append(listeNombres,(listeChiffre[i] + listeChiffre[j] + listeChiffre[k]))
 				}
 			}
 		}
