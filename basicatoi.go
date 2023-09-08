a5f0c663856beb1d2ba54b8f6f7d03292390d033
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-08 21:27:07 +0200

    basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,20 @@
+package piscine
+
+func BasicAtoi(s string) int {
+	ListeChiffre := [10]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	ListeChiffreRune := [10]rune{'0', '1', '2', '3','4', '5', '6', '7', '8', '9'}
+	var nombre int = 0
+	var puissance int = 1
+	for element := len(s)-1; element >= 0; element-- {
+		for chiffre := 0; chiffre < len(ListeChiffreRune); chiffre++ {
+			if s[element] == byte(ListeChiffreRune[chiffre]) {
+				if chiffre != '0' {
+					nombre = puissance  *  ListeChiffre[chiffre] + nombre
+				}
+				puissance = puissance * 10
+			}
+		}
+		
+	}
+	return nombre
+}
