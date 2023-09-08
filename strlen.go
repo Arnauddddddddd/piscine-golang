866f03f223b32a3fc5c9ebc5353f889eaffed696
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-08 15:17:08 +0200

    strlen

--- strlen.go
+++ strlen.go
@@ -1,11 +1,9 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func Strlen(s string) {
 	compte := 0
 	for i := 0; i < len(s); i++ {
 		compte++
 	}
-	z01.PrintRune(rune(compte))
+	return compte
 }
