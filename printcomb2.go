7fdb061b64a4d9ce2bfe9626ebd7121051f1ffd0
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-08 10:01:12 +0200

    printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -8,13 +8,13 @@ func PrintComb2() {
 		for j := 0; j < 10; j++ {
 			for k := 0; k < 10; k++ {
 				for l := 0; l < 10; l++ {
-					if i != k || j != l {
+					if (i != k || j != l) && j < i {
 						z01.PrintRune(listeChiffre[i])
 						z01.PrintRune(listeChiffre[j])
 						z01.PrintRune(' ')
 						z01.PrintRune(listeChiffre[k])
 						z01.PrintRune(listeChiffre[l])
-						if i != 7 || j != 8 || k != 9 {
+						if i != 9 || j != 8 || k != 9 || l != 9 {
 							z01.PrintRune(rune(44))
 							z01.PrintRune(' ')
 						}
