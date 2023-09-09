9430a5e1ea878a7236e9b07934260b5d9ff522b3
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-09 17:30:53 +0200

    collatz

---
+++ collatzcountdown.go
@@ -0,0 +1,15 @@
+package piscine
+
+func CollatzCountdown(start int) int {
+	nombre := 0
+	for i := 0; start != 1; {
+		if start % 2 == 0 {
+			start = start / 2
+		} else {
+			start = start * 3 + 1
+		}
+		i++
+		nombre = i
+	}
+	return nombre
+}
