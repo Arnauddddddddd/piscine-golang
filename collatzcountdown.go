8087cb2695e9ed0fce371b274b0fb9486a047e3d
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-09 17:31:40 +0200

    collatz

--- collatzcountdown.go
+++ collatzcountdown.go
@@ -3,10 +3,10 @@ package piscine
 func CollatzCountdown(start int) int {
 	nombre := 0
 	for i := 0; start != 1; {
-		if start % 2 == 0 {
+		if start%2 == 0 {
 			start = start / 2
 		} else {
-			start = start * 3 + 1
+			start = start*3 + 1
 		}
 		i++
 		nombre = i
