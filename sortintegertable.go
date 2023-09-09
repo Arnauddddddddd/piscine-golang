f63d2680b98000d057f6d24bf457aa1994c6c822
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-09 14:13:34 +0200

    sortinteger

--- sortintegertable.go
+++ sortintegertable.go
@@ -2,7 +2,7 @@ package piscine
 
 func SortIntegerTable(table []int) {
 	newTable := []int{}
-	for i := len(table)-1; i >= 0; i-- {
+	for i := len(table) - 1; i >= 0; i-- {
 		newTable = append(newTable, table[i])
 		table = table[:i]
 	}
