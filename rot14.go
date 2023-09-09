0d38dd74d66b774c0a394367400ad8ac614a91bf
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-09 15:51:11 +0200

    rot14

--- rot14.go
+++ rot14.go
@@ -1,23 +1,10 @@
-package main
-
-import "github.com/01-edu/z01"
+package piscine
 
 func Rot14(s string) string {
 	message_codee := ""
 
 	for i := 0; i < len(s); i++ {
-		message_codee += string(rune(s[i]+14))
+		message_codee += string(rune(s[i] + 14))
 	}
 	return message_codee
 }
-
-
-func main() {
-	result := Rot14("Hello! How are You?")
-
-	for _, r := range result {
-		z01.PrintRune(r)
-	}
-	z01.PrintRune('\n')
-}
-
