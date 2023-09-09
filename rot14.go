c73dd1c9feb946a124ce1488cf4c397aec56af7b
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-09 17:04:48 +0200

    rot14

--- rot14.go
+++ rot14.go
@@ -4,14 +4,14 @@ func Rot14(s string) string {
 	message_codee := ""
 	for i := 0; i < len(s); i++ {
 		if rune(s[i]) <= 90 && rune(s[i]) >= 65 {
-			if rune(s[i])+14 >= 90 {
+			if rune(s[i])+14 > 90 {
 				message_codee += string(65 + rune(s[i]) + 14 - 91)
 			} else {
 				message_codee += string(rune(s[i]) + 14)
 			}
 		}
 		if rune(s[i]) <= 122 && rune(s[i]) >= 97 {
-			if rune(s[i])+14 >= 122 {
+			if rune(s[i])+14 > 122 {
 				message_codee += string(97 + rune(s[i]) + 14 - 123)
 			} else {
 				message_codee += string(rune(s[i]) + 14)
