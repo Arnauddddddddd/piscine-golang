b1462f60a0201aad23d48c2d62a027f8e8e11125
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-11 15:09:56 +0200

    printcomb2

--- atoi.go
+++ atoi.go
@@ -3,7 +3,6 @@ package piscine
 func Atoi(s string) int {
 	entier := 0
 	signe := 1
-
 	if s[0] == '+' {
 		signe = 1
 		s = s[1:]
@@ -13,9 +12,9 @@ func Atoi(s string) int {
 		s = s[1:]
 	}
 	for _, caractere := range s {
-		entier = int(caractere - 48) + 10 * entier
+		entier = int(caractere - 48) + 10*entier
 		if caractere < 48 || caractere > 57 {
-			return 0 
+			return 0
 		}
 	}
 	return signe * entier
