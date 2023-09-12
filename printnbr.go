532ae7e63ffb81f315c807ec685e80a49c57389e
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-12 10:34:14 +0200

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -5,7 +5,6 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int) {
 	liste_nombre := []int{}
 	if n < 0 {
-		n = -n
 		z01.PrintRune(45)
 	}
 	if n == 0 {
@@ -13,6 +12,10 @@ func PrintNbr(n int) {
 	} else {
 		for n > 0 {
 			liste_nombre = append(liste_nombre, n%10)
+			n = n / 10
+		}
+		for n < 0 {
+			liste_nombre = append(liste_nombre, -(n%10))
 			n = n / 10
 		}
 		for i := len(liste_nombre) - 1; i >= 0; i-- {
