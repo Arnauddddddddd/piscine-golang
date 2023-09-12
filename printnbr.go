5afea6d15eb7e0c0d4124e1d465d950106c5e40c
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-12 10:28:03 +0200

    printnbr

--- printnbr.go
+++ printnbr.go
@@ -4,32 +4,19 @@ import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
 	liste_nombre := []int{}
-	var signe int
-	multiplicateur := 1
-	m := n
-	var entier int
 	if n < 0 {
-		signe = -1
-		m, n = -n, -n
-	}
-	if signe == -1 {
+		n = -n
 		z01.PrintRune(45)
 	}
 	if n == 0 {
 		z01.PrintRune('0')
 	} else {
-		for m > 10 {
-			m = m / 10
-			multiplicateur *= 10
+		for n > 0 {
+			liste_nombre = append(liste_nombre, n%10)
+			n = n/10
 		}
-		for n > 1 {
-			entier = n / multiplicateur
-			liste_nombre = append(liste_nombre, entier)
-			n = n - entier*multiplicateur
-			multiplicateur /= 10
-		}
-		for _, i := range liste_nombre {
-			z01.PrintRune(rune(i + 48))
+		for i := len(liste_nombre)-1; i >= 0; i-- {
+			z01.PrintRune(rune(liste_nombre[i] + 48))
 		}
 	}
 }
