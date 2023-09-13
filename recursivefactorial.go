93198e1dbb1d672893c7f315757584ea9bee5eb1
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-13 09:43:49 +0200

    recursivefactorial

---
+++ recursivefactorial.go
@@ -0,0 +1,15 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	if nb == 0 {
+		return 1
+	}
+	if nb < 0 || nb >= 99 {
+		return 0
+	}
+	if nb < 2 {
+		return nb
+	} else {
+		return nb * IterativeFactorial(nb-1)
+	}
+}
