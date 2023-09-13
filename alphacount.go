3ad1e7933fa550a595c96087e5242bbaa7c9dfc4
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-13 23:40:33 +0200

    alphacount

--- alphacount.go
+++ alphacount.go
@@ -3,7 +3,7 @@ package piscine
 func AlphaCount(s string) int {
 	tab := []rune(s)
 	compteur := 0
-	for i:= range tab {
+	for i := range tab {
 		if (tab[i] >= 65 && tab[i] <= 90) || (tab[i] >= 97 && tab[i] <= 122) {
 			compteur += 1
 		}
