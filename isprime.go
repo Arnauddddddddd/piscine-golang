394d79d627baedcc768646d278b680ce871a6db4
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-13 18:18:11 +0200

    isprime

--- isprime.go
+++ isprime.go
@@ -10,10 +10,8 @@ func IsPrime(nb int) bool {
 	if nb == 2 || nb == 3 || nb == 5 || nb == 7 {
 		return true
 	}
-	for i := 11; i < nb || i > 500; i++ {
-		if !(nb%2 == 0 || nb%3 == 0 || nb%5 == 0 || nb%7 == 0) {
-			return false
-		}
+	if !(nb%2 == 0 || nb%3 == 0 || nb%5 == 0 || nb%7 == 0) {
+		return false
 	}
 	return true
 }
