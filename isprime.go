cfe0fcdb2157cba361c753ddf1215dd1a4212cd3
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-13 19:08:54 +0200

    isprime

--- isprime.go
+++ isprime.go
@@ -1,17 +1,16 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	if nb < 0 {
+	if nb <= 1 {
 		return false
 	}
 	if nb == 1 {
 		return false
 	}
-	if nb == 2 || nb == 3 || nb == 5 || nb == 7 {
-		return true
-	}
-	if nb%2 == 0 || nb%3 == 0 || nb%5 == 0 || nb%7 == 0 || nb%10 == 0 {
-		return false
+	for i := 2; i < nb-1; i++ {
+		if nb%i == 0 {
+			return false
+		}
 	}
 	return true
 }
