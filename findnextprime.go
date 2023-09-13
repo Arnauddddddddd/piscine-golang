6d290416a5e2bc0ecdfe7825a965e7c6d8b73b54
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-13 20:39:31 +0200

    findnextprime

--- findnextprime.go
+++ findnextprime.go
@@ -5,9 +5,9 @@ func FindNextPrime(nb int) int {
 	if nb <= 1 {
 		return 2
 	}
-	for i := 2; i < nb/2+1; i++ {
+	for i := 2; i < Sqrt(nb)+1; i++ {
 		if nb%i == 0 {
-			tab = 1
+			tab += 1
 		}
 	}
 	if tab == 0 {
