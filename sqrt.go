1afde7b04d120d838a040438569f6ab385e78585
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-13 12:07:26 +0200

    sqrt

--- sqrt.go
+++ sqrt.go
@@ -1,12 +1,20 @@
 package piscine
 
 func Sqrt(nb int) int {
-	ls_nb := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}
-	ls_sq := []int{1, 0, 0, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 4}
-	for i, j := range ls_nb {
-		if nb == j {
-			return ls_sq[i]
-		}
+	if nb <= 0 {
+		return 0
 	}
-	return 0
+	p := 0
+	i := 0
+	if nb == 3 {
+		return 0
+	}
+	for true {
+		if nb >= i*(i-1) && nb <= i*(i+1) || i*i == nb {
+			p = i
+			break
+		}
+		i++
+	}
+	return p
 }
