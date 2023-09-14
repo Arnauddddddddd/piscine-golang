ca7332605ca65f598c642ea1835e6c3b25fd0459
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-14 21:06:05 +0200

    basicjoin

--- basicjoin.go
+++ basicjoin.go
@@ -3,7 +3,10 @@ package piscine
 func BasicJoin(elems []string) string {
 	txt := ""
 	for i := range elems {
-		txt += elems[i] + " "
+		txt += elems[i]
+		if elems[i] != elems[len(elems)-1] {
+			txt += " "
+		}
 	}
 	return txt
 }
