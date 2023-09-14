bc9da717d1ef4a7b2afb2f51aca77bf76f77b069
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-14 08:59:25 +0200

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for i := range s {
-		if (rune(s[i]) < 97 || rune(s[i]) > 122) && (rune(s[i]) < 65 || rune(s[i]) > 90) {
+		if (rune(s[i]) < 97 || rune(s[i]) > 122) && (rune(s[i]) < 65 || rune(s[i]) > 90) && (rune(s[i]) < 48 || rune(s[i]) > 57) {
 			return false
 		}
 	}
