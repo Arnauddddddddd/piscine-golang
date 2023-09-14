d04dc9e9d4634f5de16b8ab15354649964716d78
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-14 15:55:57 +0200

    printnbrinorder

--- printnbrinorder.go
+++ printnbrinorder.go
@@ -4,6 +4,10 @@ import "github.com/01-edu/z01"
 
 func PrintNbrInOrder(n int) {
 	liste_nombre := []int{}
+	signe := 0
+	if n < 0 {
+		signe = -1
+	}
 	if n == 0 {
 		z01.PrintRune('0')
 	} else {
@@ -15,11 +19,11 @@ func PrintNbrInOrder(n int) {
 			liste_nombre = append(liste_nombre, -(n % 10))
 			n = n / 10
 		}
-	}
-	for i := 0; i < len(liste_nombre); i++ {
-		z01.PrintRune(rune(liste_nombre[i] + 48))
-	}
-	if n < 0 {
-		z01.PrintRune(45)
+		for i := 0; i <= len(liste_nombre) - 1; i++ {
+			z01.PrintRune(rune(liste_nombre[i] + 48))
+		}
+		if signe == -1 {
+			z01.PrintRune(45)
+		}
 	}
 }
