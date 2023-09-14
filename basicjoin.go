8118899c339d66dcee6a81cea58e43760ead6390
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-14 21:12:29 +0200

    basicjoin

--- basicjoin.go
+++ basicjoin.go
@@ -4,7 +4,7 @@ func BasicJoin(elems []string) string {
 	txt := ""
 	for i := range elems {
 		txt += elems[i]
-		if !(elems[i] == elems[len(elems)-1]) {
+		if !(elems[i] != elems[len(elems)-1]) {
 			txt += " "
 		}
 	}
