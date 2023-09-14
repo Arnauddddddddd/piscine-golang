aae9ed601385145e52111a4787ae394a0271b938
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-14 13:31:42 +0200

    toupper

---
+++ toupper.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ToUpper(s string) string {
+	tab := []rune(s)
+	for i := range tab {
+		if tab[i] >= 97 && tab[i] <= 122 {
+			tab[i] = rune(s[i]-32)
+		}
+	}
+	s2 := ""
+	for i := 0; i < len(tab); i++ {
+		s2 += string(tab[i])
+	}
+	return s2
+}
