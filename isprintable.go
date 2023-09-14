383aec512b9a6f236dfce4087e288d5ac609653a
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-14 11:11:16 +0200

    isprintable

--- isprintable.go
+++ isprintable.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsPrintable(s string) bool {
 	for i := range s {
-		if rune(s[i]) < 33 || rune(s[i]) > 126 {
+		if rune(s[i]) < 32 || rune(s[i]) > 126 {
 			return false
 		}
 	}
