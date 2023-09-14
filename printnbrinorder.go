d349d9ef5e176088a80b6cfe159816092d9d57b3
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-14 13:56:26 +0200

    printnbrinorder

---
+++ printnbrinorder.go
@@ -0,0 +1,25 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	liste_nombre := []int{}
+	if n < 0 {
+		z01.PrintRune(45)
+	}
+	if n == 0 {
+		z01.PrintRune('0')
+	} else {
+		for n > 0 {
+			liste_nombre = append(liste_nombre, n%10)
+			n = n / 10
+		}
+		for n < 0 {
+			liste_nombre = append(liste_nombre, -(n % 10))
+			n = n / 10
+		}
+		for i := 0; i < len(liste_nombre); i++ {
+			z01.PrintRune(rune(liste_nombre[i] + 48))
+		}
+	}
+}
