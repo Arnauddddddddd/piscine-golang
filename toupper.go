1a55e8bf086509063f2a4c72bcfff59787fc4436
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-14 13:32:37 +0200

    toupper

--- toupper.go
+++ toupper.go
@@ -4,7 +4,7 @@ func ToUpper(s string) string {
 	tab := []rune(s)
 	for i := range tab {
 		if tab[i] >= 97 && tab[i] <= 122 {
-			tab[i] = rune(s[i]-32)
+			tab[i] = rune(s[i] - 32)
 		}
 	}
 	s2 := ""
