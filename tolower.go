8fa6b362c1d96e6a8ce94660eb1a266e1939c36f
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-14 13:37:33 +0200

    tolower

---
+++ tolower.go
@@ -0,0 +1,13 @@
+func ToLower(s string) string {
+	tab := []rune(s)
+	for i := range tab {
+		if tab[i] >= 65 && tab[i] <= 90 {
+			tab[i] = rune(s[i] + 32)
+		}
+	}
+	s2 := ""
+	for i := 0; i < len(tab); i++ {
+		s2 += string(tab[i])
+	}
+	return s2
+}	
