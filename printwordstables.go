6e5bf452e27174e0c5ca8d3e833ed254f1283945
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-15 12:51:34 +0200

    printwordstables

--- printwordstables.go
+++ printwordstables.go
@@ -4,7 +4,7 @@ import "github.com/01-edu/z01"
 
 func PrintWordsTables(a []string) {
 	for i := 0; i < len(a); i++ {
-		for j := 0; j < len(a[i]); j++{
+		for j := 0; j < len(a[i]); j++ {
 			z01.PrintRune(rune(byte(a[i][j])))
 		}
 		z01.PrintRune('\n')
