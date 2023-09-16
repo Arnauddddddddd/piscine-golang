4c5cae3807e31075fb597b15e7b6b1cb490a3496
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-16 14:03:51 +0200

    split

--- split.go
+++ split.go
@@ -1,7 +1,6 @@
 package piscine
 
 func Split(s, sep string) []string {
-	test := []string{}
 	tab_indice := []int{}
 	tab := []byte(s)
 	sepa := []byte(sep)
@@ -17,18 +16,16 @@ func Split(s, sep string) []string {
 			tab_indice = append(tab_indice, i)
 		}
 	}
-	for i := len(s) - 1; i > 0; i-- {
-		for j := len(tab_indice) - 1; j > 0; j-- {
-			s = s[:tab_indice[j]-1] + s[(tab_indice[j]):]
+	for i := 0; i < len(s); i++ {
+		for j := 0; j < len(tab_indice); j++ {
+			for k := 0; k < len(sepa); k++ {
+				tab[tab_indice[j]+k] = ' '
+			}
 		}
 	}
 	txt := ""
 	for i := 0; i < len(tab); i++ {
 		txt += string(tab[i])
 	}
-	return test
+	return SplitWhiteSpaces(txt)
 }
-
-
-
-
