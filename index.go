98df99c6ab114c3a5474f6c7a98d8713d363487c
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-16 14:33:00 +0200

    index

--- index.go
+++ index.go
@@ -3,23 +3,18 @@ package piscine
 func Index(s string, toFind string) int {
 	s_tab := []byte(s)
 	tofind_tab := []byte(toFind)
-	var index int
+	var index int = -1
 	var compteur int
-	var test bool = false
 	for i := 0; i < len(s_tab)-len(tofind_tab); i++ {
 		for j := 0; j < len(tofind_tab); j++ {
 			if s_tab[i+j] == tofind_tab[j] {
 				compteur++
-				test = true
 			}
 		}
 		if compteur == len(tofind_tab) {
-			index = i
+			return i
 		}
 		compteur = 0
-	}
-	if !test {
-		index = -1
 	}
 	return index
 }
