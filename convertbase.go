f91666cb105d37f1034f7b59b5be00875e810fd5
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-16 16:06:52 +0200

    convertbase

--- convertbase.go
+++ convertbase.go
@@ -1,15 +1,33 @@
 package piscine
 
 func ConvertBase(nbr, baseFrom, baseTo string) string {
-	var nombre int = 0
+	var n int = 0
 	if baseFrom == "01" {
 		tab_nbr := []byte(nbr)
 		calcul := 1
 		for i := len(tab_nbr) - 1; i >= 0; i-- {
 			if tab_nbr[i] == '1' {
-				nombre += calcul
+				n += calcul
 			}
 			calcul *= 2
+		}
+		liste_nombre := []int{}
+		txt := ""
+		if n == 0 {
+			return "0"
+		} else {
+			for n > 0 {
+				liste_nombre = append(liste_nombre, n%10)
+				n = n / 10
+			}
+			for n < 0 {
+				liste_nombre = append(liste_nombre, -(n % 10))
+				n = n / 10
+			}
+			for i := len(liste_nombre) - 1; i >= 0; i-- {
+				txt += string((rune(liste_nombre[i] + 48)))
+			}
+			return txt
 		}
 	}
 	return nbr
