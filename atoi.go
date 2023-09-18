2758a7b591715476bafb31ef70026756cf803900
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-18 09:35:38 +0200

    atoi

--- atoi.go
+++ atoi.go
@@ -14,12 +14,11 @@ func Atoi(s string) int {
 		signe = -1
 		s = s[1:]
 	}
-	for i := 0; i < len(s); i++ {
-		if rune(s[i]) < 48 || rune(s[i]) > 57 {
+	for _, i := range s {
+		if rune(i) < 48 || rune(i) > 57 {
 			return 0
 		}
-		entier = 10 * entier
-		entier += int(s[i] - 48)
+		entier = 10 * entier + int(i - 48)
 	}
 	return signe * entier
 }
