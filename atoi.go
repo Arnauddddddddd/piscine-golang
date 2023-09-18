b76ea942ad81b1fd1ecca51f37d67c1a937c232c
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-18 09:46:36 +0200

    atoi

--- atoi.go
+++ atoi.go
@@ -3,19 +3,20 @@ package piscine
 func Atoi(s string) int {
 	entier := 0
 	signe := 1
-	if len(s) == 0 {
+	s2 := []rune(s)
+	if len(s2) == 0 {
 		return 0
 	}
-	if s[0] == '+' {
+	if s2[0] == '+' {
 		signe = 1
-		s = s[1:]
+		s2 = s2[1:]
 	}
-	if s[0] == '-' {
+	if s2[0] == '-' {
 		signe = -1
-		s = s[1:]
+		s2 = s2[1:]
 	}
-	for _, i := range s {
-		if rune(i) < 48 || rune(i) > 57 {
+	for _, i := range s2 {
+		if i < 48 || i > 57 {
 			return 0
 		}
 		entier = 10*entier + int(i-48)
