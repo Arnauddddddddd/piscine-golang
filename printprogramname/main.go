04d771642a92ff18e26830963da2c86de955b88f
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-18 10:02:36 +0200

    printprogramname

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"os"
+
 	"github.com/01-edu/z01"
 )
 
 func main() {
 	tab := []rune(os.Args[0])
 	for i := range tab {
-		z01.PrintRune(tab[i])
+		if i > 1 {
+			z01.PrintRune(tab[i])
+		}
 	}
 	z01.PrintRune('\n')
 }
