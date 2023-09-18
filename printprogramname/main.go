249333008680e5416101ebae9aefe50917d00ff5
parnaud <arnaud.pfeillefort@ynov.com>
2023-09-18 09:11:05 +0200

    convertbase

--- printprogramname/main.go
+++ printprogramname/main.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"os"
+    "os"
 
-	"github.com/01-edu/z01"
+    "github.com/01-edu/z01"
 )
 
 func main() {
-	arguments := os.Args
-	name := arguments[0]
-	runes := []rune(name)
-	for i := range runes {
-		z01.PrintRune(runes[i])
-	}
-	z01.PrintRune('\n')
+    arg := os.Args
+    for _, i := range arg[0][2:] {
+        z01.PrintRune(i)
+    }
+    z01.PrintRune('\n')
 }
